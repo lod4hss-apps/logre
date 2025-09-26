b6d649c372f6784c4e7842bc5b539b1dafb1ed34
Gaétan Muck <gaetan.muck@gmail.com>
2025-09-26 09:22:04 +0200

    sparql editor error message when no data bundle

--- src/pages/sparql-editor.py
+++ src/pages/sparql-editor.py
@@ -16,81 +16,86 @@ menu()
 
 try:
 
-    # Title
-    st.markdown('# SPARQL Editor')
-    st.markdown('[More about the edior in the Documentation FAQ](/documentation#what-type-of-queries-can-i-write-in-the-sparql-editor)')
-    
-    st.markdown('')
-
     # From state
     sparql_queries = state.get_sparql_queries()
     sparql_query_name = state.get_sparql_query()
     data_bundle = state.get_data_bundle()
 
-    # Find the selected one
-    sparql_queries_names = [sq[0] for sq in sparql_queries]
-    index = sparql_queries_names.index(sparql_query_name) if sparql_query_name else 0
+    # Make verifications
+    if not data_bundle:
+        st.warning('No Data Bundle selected')
+    else:
+        
+        # Title
+        st.markdown('# SPARQL Editor')
+        st.markdown('[More about the edior in the Documentation FAQ](/documentation#what-type-of-queries-can-i-write-in-the-sparql-editor)')
+        
+        st.markdown('')
 
-    # Allow user to change the selected queries
-    with st.container(horizontal=True, vertical_alignment='bottom'):
-        sparql_query_name = st.selectbox('SPARQL query', options=sparql_queries_names, index=index,width=300, on_change=state.set_sparql_query, args=(sparql_query_name,), help="[Can I save a specific query?](/documentation#can-i-save-a-specific-query)")
+        # Find the selected one
+        sparql_queries_names = [sq[0] for sq in sparql_queries]
+        index = sparql_queries_names.index(sparql_query_name) if sparql_query_name else 0
 
-        # And have a delete button for this query
-        if st.button('', icon=':material/delete:', type='tertiary'):
-            def callback_delete_query(sq_name: str) -> None:
-                state.delete_sparql_query(sq_name)
-                state.set_toast('Query removed', icon=':material/delete:')
-            dialog_confirmation(f"You are about to delete the query *{sparql_query_name}*.", callback=callback_delete_query, sq_name=sparql_query_name)
+        # Allow user to change the selected queries
+        with st.container(horizontal=True, vertical_alignment='bottom'):
+            sparql_query_name = st.selectbox('SPARQL query', options=sparql_queries_names, index=index,width=300, on_change=state.set_sparql_query, args=(sparql_query_name,), help="[Can I save a specific query?](/documentation#can-i-save-a-specific-query)")
 
-    # Get the query content
-    sparql_query_content = sparql_queries[sparql_queries_names.index(sparql_query_name)][1] if sparql_query_name else ''
+            # And have a delete button for this query
+            if st.button('', icon=':material/delete:', type='tertiary'):
+                def callback_delete_query(sq_name: str) -> None:
+                    state.delete_sparql_query(sq_name)
+                    state.set_toast('Query removed', icon=':material/delete:')
+                dialog_confirmation(f"You are about to delete the query *{sparql_query_name}*.", callback=callback_delete_query, sq_name=sparql_query_name)
 
-    # Display prefixes for user to know
-    prefixes_str = '`, `'.join([p.short for p in data_bundle.prefixes])
-    st.markdown('Available prefixes are: `' + prefixes_str + '`')
+        # Get the query content
+        sparql_query_content = sparql_queries[sparql_queries_names.index(sparql_query_name)][1] if sparql_query_name else ''
 
-    # Code editor
-    editor = code_editor(
-        lang="sparql",
-        code=sparql_query_content,
-        height="400px",
-        buttons=[{"name": "Run", "hasText": True, "alwaysOn": True,"style": {"top": "350px", "right": "0.4rem"}, "commands": ["submit"]}]
-    )
+        # Display prefixes for user to know
+        prefixes_str = '`, `'.join([p.short for p in data_bundle.prefixes])
+        st.markdown('Available prefixes are: `' + prefixes_str + '`')
 
-    st.write('')
+        # Code editor
+        editor = code_editor(
+            lang="sparql",
+            code=sparql_query_content,
+            height="400px",
+            buttons=[{"name": "Run", "hasText": True, "alwaysOn": True,"style": {"top": "350px", "right": "0.4rem"}, "commands": ["submit"]}]
+        )
 
-    # When submit button is clicked
-    if editor['type'] == 'submit' and editor['id'] != state.get_last_executed_sparql_id():
+        st.write('')
 
-        # Run the query
-        result = data_bundle.endpoint.run(editor['text'], data_bundle.prefixes)
-        state.set_last_executed_sparql_id(editor['id'])
+        # When submit button is clicked
+        if editor['type'] == 'submit' and editor['id'] != state.get_last_executed_sparql_id():
 
-        # If there is a result
-        if result != None:
-            df = pd.DataFrame(result)
+            # Run the query
+            result = data_bundle.endpoint.run(editor['text'], data_bundle.prefixes)
+            state.set_last_executed_sparql_id(editor['id'])
 
-            option_line = st.container(horizontal=True, horizontal_alignment='distribute', vertical_alignment='bottom')
+            # If there is a result
+            if result != None:
+                df = pd.DataFrame(result)
 
-            # Option line: title, shape, and buttons
-            with option_line.container(horizontal=True, horizontal_alignment='left', vertical_alignment='bottom'):
-                st.markdown("### Response", width='content')
-                st.markdown(f"Shape: {df.shape[0]}x{df.shape[1]}", width='content')
+                option_line = st.container(horizontal=True, horizontal_alignment='distribute', vertical_alignment='bottom')
 
-            # Options buttons: download and save query
-            with option_line.container(horizontal=True, horizontal_alignment='right', vertical_alignment='bottom'):
-                st.download_button('Download CSV', data=df.to_csv(index=False), file_name="logre-download.csv", mime="text/csv", icon=':material/download:')
-                # if st.button('Save query', kwargs={'text': editor['text']}, icon=':material/reorder:'): 
-                st.button('Save query', icon=':material/reorder:', on_click=dialog_query_name, kwargs={'query_text': editor['text']})
+                # Option line: title, shape, and buttons
+                with option_line.container(horizontal=True, horizontal_alignment='left', vertical_alignment='bottom'):
+                    st.markdown("### Response", width='content')
+                    st.markdown(f"Shape: {df.shape[0]}x{df.shape[1]}", width='content')
 
-            # Display query result
-            st.dataframe(df, hide_index=True)
+                # Options buttons: download and save query
+                with option_line.container(horizontal=True, horizontal_alignment='right', vertical_alignment='bottom'):
+                    st.download_button('Download CSV', data=df.to_csv(index=False), file_name="logre-download.csv", mime="text/csv", icon=':material/download:')
+                    # if st.button('Save query', kwargs={'text': editor['text']}, icon=':material/reorder:'): 
+                    st.button('Save query', icon=':material/reorder:', on_click=dialog_query_name, kwargs={'query_text': editor['text']})
 
-        # When there is no result: a insert/delete query
-        else:
-            # Inform user that the request went through
-            state.set_toast('Query executed', icon=':material/done:')
-            st.rerun()
+                # Display query result
+                st.dataframe(df, hide_index=True)
+
+            # When there is no result: a insert/delete query
+            else:
+                # Inform user that the request went through
+                state.set_toast('Query executed', icon=':material/done:')
+                st.rerun()
 
 except HTTPError as err:
     message = get_HTTP_ERROR_message(err)
