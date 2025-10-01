d1cb944066a7d746eb6707c926df831b03de1f1f
Gaétan Muck <gaetan.muck@gmail.com>
2025-10-01 11:15:35 +0200

    bat file correction

--- logre.bat
+++ logre.bat
@@ -5,12 +5,19 @@ SET PIPENV_NAME=pipenv_logre
 SET REQUIREMENTS_FILE=requirements.txt
 
 REM [LOGRE] Check Python installation...
-@where python >nul 2>nul
-if %ERRORLEVEL% neq 0 (
-    @echo "[ERROR] Python ('python') not found (installed or in PATH)."
-    @echo "[ERROR] Go to https://www.python.org/downloads/ to install it."
-    @pause
-    @goto end
+@where python >nul 2>&1
+if %ERRORLEVEL%==0 (
+    set PYEXEC=python
+) else (
+    @where py >nul 2>&1
+    if %ERRORLEVEL%==0 (
+        set PYEXEC=py
+    ) else (
+        @echo "[ERROR] Python ('python') not found (installed or in PATH)."
+        @echo "[ERROR] Go to https://www.python.org/downloads/ to install it."
+        @pause
+        @goto end
+    )
 )
 
 REM [LOGRE] Check Git installation...
@@ -27,7 +34,7 @@ REM [LOGRE] Updating code base...
 
 REM [LOGRE] Create virtual environment...
 if not exist "pipenv_logre\Scripts\activate.bat" (
-    @python -m venv %PIPENV_NAME%
+    @%PYEXEC% -m venv %PIPENV_NAME%
 )
 
 REM [LOGRE] Activate virtual environment...
@@ -38,10 +45,11 @@ REM [LOGRE] Installing dependencies...
 
 REM [LOGRE Install GitHub Dependencies]
 @git clone https://github.com/lod4hss-apps/graphly.git
-@cd graphly; pip install .
+@cd graphly && pip install .
 
 REM [LOGRE] Starting Logre...
-@python -m streamlit run src\server.py
+@%PYEXEC% -m streamlit run src\server.py
 @start http://localhost:8501
 
 :end
+pause
